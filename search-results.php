d789d7aead020468e90c892f4cf77b2ee2fc9a39
davistroy <troy.davis@hotmail.com>
2021-02-03 11:11:11 -0500

    update search

---
+++ search-results.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0 maximum-scale=1.0, user-scalable=0" />
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DR7Z07WWX2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DR7Z07WWX2');
+</script>
+
+    <!-- Include for Font Awesome icons -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+      integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
+      crossorigin="anonymous"
+    />
+
+    <!-- Include for Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/smallscreen.css" />
+
+    <title>Silver Comet ARS</title>
+  </head>
+
+  <body>
+    <?php include 'header.html'; ?>
+
+    <!-- Main body of page -->
+    <div class="main">
+      <script async src="https://cse.google.com/cse.js?cx=3292a0e472f351a59"></script>
+      <div class="gcse-searchresults-only"></div>
+    </div>
+
+    <?php include 'footer.html'; ?>
+  </body>
+</html>
